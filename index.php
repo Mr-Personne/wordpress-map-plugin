f005b9028e93fa994ecab2ced2a96aca6f741c7a
Mr-Personne <h.sacha07@gmail.com>
2020-02-25 09:39:53 +0000

    added basique js map

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   Version: 1.0.0
  */
 
- //help : https://karac.ch/blog/creer-ses-propres-shortcodes-wordpress
+//help : https://karac.ch/blog/creer-ses-propres-shortcodes-wordpress
 
 //  function shortcode_bienvenue(){
 //      return "<h2>Bienvenue chez blogBreakers !!!! </h2>";
@@ -34,36 +34,36 @@
 
 //  add_shortcode('bienvenueV2', 'shortcode_bienvenueV2');
 
- ?>
+?>
 
 
 <!-- section map -->
 <!-- https://www.sitepoint.com/mapping-geocoder-php-leaflet-js/ -->
 <!-- https://packagist.org/packages/geocoder-php/google-maps-provider -->
-<?php
-// require 'vendor/autoload.php';
-?>
-<!DOCTYPE html>
-<html>
-<head>
-    <title>A simple map with Geocoder PHP and Leaflet.js</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
-</head>
-<body>
-<div class="container">
-    <div class="row">
-        <div class="col-lg-12 page-header">
-            <h1 id="header">A simple map with Geocoder PHP and Leaflet.js</h1>
-        </div>
-        <div class="row-fluid">
-            <div class="col-lg-8">
+<!-- https://leafletjs.com/examples/quick-start/ -->
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==" crossorigin="" />
+<!-- Make sure you put this AFTER Leaflet's CSS -->
+<script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js" integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew==" crossorigin=""></script>
+<style>
+    #mapid {
+        height: 500px;
+    }
+</style>
+<div id="mapid"></div>
 
-            </div>
-        </div>
-    </div><!-- /row -->
-</div> <!-- /container -->
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
-</body>
-</html>
+<script>
+    let longitude = 6.1518422;
+    let latitude = 47.6369051;
+    var mymap = L.map('mapid').setView([latitude,  longitude], 15);
+    var marker = L.marker([latitude, longitude]).addTo(mymap);
+    // marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+    // ACCESS_TOKEN = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw';
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw'
+    }).addTo(mymap);
+</script>
